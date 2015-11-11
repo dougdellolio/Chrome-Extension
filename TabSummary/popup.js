c396e1865f6a65924f0787d95fcbb39d640b954f
ddellolio <doug.dellolio@gmail.com>
2015-11-11 10:51:40 -0500

    start

--- TabSummary/popup.js
+++ TabSummary/popup.js
@@ -1,8 +1,30 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+//this will talk to the API and get windows and tabs
+function sessionOverview() {
+   chrome.tabs.query({}, function(tabs) {
+    addToWindowList(tabs);
+   });
+
+  //creates the overlay div
+  var success = document.createElement('div');
+  success.classList.add('overlay');
+  success.textContent = 'Session Overview';
+  document.body.appendChild(success);
+  setTimeout(function() { success.classList.add('visible'); }, 10);
+}
+
+//adds the window list to its div
+function addToWindowList(list){
+  $('#windows').append("Number of Tabs Open: " + list.length);
+  var text = "<ul type='circle'>";
+
+  for (i = 0; i < list.length; i++) { 
+      text += "<li>" + "Session ID of Tab " + (i+1) + " = " + list[i].url + "</li>";
+  }
+
+  $('#windows').append(text + "</ul>");
+}
+
 
 document.addEventListener('DOMContentLoaded', function () {
-
-  });
+  sessionOverview();
 });
