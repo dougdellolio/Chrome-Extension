ede64a999379f026431db96a184afe876c61abf3
ealouie <ellena.louie@gmail.com>
2015-12-05 15:58:30 -0500

    removed window div

--- TabSummary/popup.js
+++ TabSummary/popup.js
@@ -5,7 +5,24 @@ function sessionOverview() {
 }
 
 function addToTabList(list){
-  $('#tabs').append("<br>Number of Tabs Open: " + list.length);
+  $('#tabs').append("Number of Tabs Open: " + list.length);
+  var text = "<ul type='circle'>";
+
+  for (i = 0; i < list.length; i++) { 
+      text += "<li>" + list[i].url + "</li>";
+  }
+
+  chrome.storage.sync.set({ "data" : text }, function() {
+      if (chrome.runtime.error) {
+        console.log("Runtime error.");
+      }
+  });
+
+  $('#tabs').append(text + "</ul>");
+}
+
+function addToTabList(list){
+  $('#tabs').append("Number of Tabs Open: " + list.length);
   var text = "<ul type='circle'>";
 
   for (i = 0; i < list.length; i++) { 
@@ -35,12 +52,35 @@ function temporarilySaveData(){
   });
 }
 
+function printAll(indexOne){
+  //array[indexOne.charAt(6)] is the entire list from the button you pressed
+  //splitter[parseInt(indexOne.charAt(7))+1] splits at <li> to get the link of the button you pressed
+  //we get the whole html and then replace the link with nothing
+  //get the data from storage and than replace it with the new removed link list
+
+  chrome.storage.sync.get({list: []}, function(data) {
+    var array = data.list;
+    var finalArray =[ array[0] ];
+    var splitter = array[indexOne.charAt(6)].split("<li>").concat("<li>"); //returns first number after button from button 00
+  
+    array[indexOne.charAt(6)] = array[indexOne.charAt(6)].replace('<li>' + splitter[parseInt(indexOne.charAt(7))+1], "");
+
+    chrome.storage.sync.get({list: []}, function(data) {
+      var arrayOriginal = data.list;
+      arrayOriginal.unshift(array);
+
+       chrome.storage.sync.set({list:array}, function() {
+           console.log("Removed from list");
+       });
+    });
+  });
+}
+
 function addToHistory(text) {
   chrome.storage.sync.get({list: []}, function(data) {
     var array = data.list;
     array.unshift(text);
-    console.log(array);
-    
+
      chrome.storage.sync.set({list:array}, function() {
          console.log("added to list");
      });
@@ -59,10 +99,29 @@ document.addEventListener('DOMContentLoaded', function () {
       var result = str.match(/<li>(.*?)<\/li>/g).map(function(val){
         return val.replace(/<\/?li>/g,'');
       });
-      $('#history').append('<br><b>' + getTime[0] + '</b>');
+
+      $('#history').append(getTime[0]);
+      
+      var linkId;
+      var buttonId;
+      var button;
+      
       for(i = 0 ; i < result.length; i++) {
-        $('#history').append('<ul class="a"><li type = "circle"><a href="'+ result[i] +'">'+ result[i] +'</a></li></ul>');
+        buttonId = "button" + element + i;
+        linkId = i;
         
+        $('#history').append('<li id=' + linkId + '><a href="' + result[i] + '">' + result[i] + '</a>&nbsp&nbsp<button id=' + buttonId + '>x</button></li>');
+      
+        button = document.getElementById(buttonId);
+
+        if(typeof window.addEventListener == 'function') {
+          (function(button) {
+            button.addEventListener('click', function() {
+              printAll(button.id, result[i]);
+              console.log(button);
+            });
+          })(button);
+        }
       }
 
       $('#history').append("<br>");
@@ -72,8 +131,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
   sessionOverview();
 });
-
-//when window closes temporarily save data from the DOM
 chrome.windows.onRemoved.addListener(function(windowId) {
   temporarilySaveData();
 });
